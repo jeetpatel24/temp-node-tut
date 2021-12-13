1dcd976d0aa8e88a01d098ce22166fdb72a06668
jeetpatel24 <friend.jeet24@gmail.com>
2021-12-13 10:29:28 +0530

    app.use

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,57 +1,26 @@
 const express = require('express');
 const app = express();
+const logger = require('./logger')
 
-var {products} = require('./data');
+// req =>  middleware => res
+
+app.use(logger); //to automatically use logger in every api
 
 app.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+    res.send('<h1>Home Page</h1>');
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About Page</h1>');
 })
 
 app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product) => {
-        const {id, name, image} = product;
-        return {id, name, image};
-    })
-
-    res.json(newProducts);
+    res.send('<h1>Products</h1>');
 })
 
-app.get('/api/product/:productId', (req, res) => {
-    const singleProduct = products.find((product) => 
-        product.id === Number(req.params.productId)
-    );
-
-    if(!singleProduct) {
-        res.status(404).send('Product does not exit');
-    }
-    res.json(singleProduct);
+app.get('/api/items', (req, res) => {
+    res.send('<h1>Items</h1>');
 })
-
-app.get('/api/product/:productId/reviews/:reviewId', (req, res) => {
-    res.send('hello world');
-})
-
-app.get('/api/v1/query', (req, res) => {
-    let sortedProducts = [...products];
-    const {search, limit} = req.query;
-
-    if(search) {
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.name.startsWith(search);
-        })
-    }
-
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-
-    if(sortedProducts.length < 1) {
-        // res.status(200).send('no prodcts matched your search');
-        return res.status(200).send({success:true, data:[]});
-    }
-
-    return res.json(sortedProducts);
-})
-
-
 
 app.listen(5000, ()=> {
     console.log(5000, 'server is listening on port 5000...');
